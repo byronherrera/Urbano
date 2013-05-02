37dafa41d8b0e62c2e380627854d5cb54edf3ab6
byron herrera nafta <byron@3w.nafta.ec>
2013-05-02 18:17:37 -0500

    fin dia

--- pruebas/js/urbanotrack.js
+++ pruebas/js/urbanotrack.js
@@ -1,16 +1,32 @@
 $(document).ready(function () {
     //alert("Your location is: " + geoplugin_countryName() + ", " + geoplugin_region() + ", " + geoplugin_city());
-    var country = getCountry();
-
+    inic()
+    getCountry();
 });
-//cierra document ready
+//fin document ready
+
 
 function getCountry() {
-
     var country = geoplugin_countryName();
-    console.log (country);
     $('#country').val(country);
+    //muestra o oculta destino
+    muestraDestino (country)
+}
+//inicializa parametros y crea evento change del combo box el cual muestra o oculta dependiendo del pais.
+function inic() {
+    $("#destino").hide();
+    $('#country').change(function() {
+        muestraDestino ($("#country option:selected").val())
+    });
 
 }
 
+//oculta o muestra destino dependiendo del pais que ingrese.
+function muestraDestino (country){
+    if (country == 'Ecuador') {
+        $("#destino").show('slow');
+    } else {
+        $("#destino").hide();
+    }
+}
 
